8b00eeedf365c325b53f8e95748ed3246a5c028f
Jeff Cameron <jeffgcameron@gmail.com>
2021-10-19 00:27:19 -0600

    dropdown

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import PermContactCalendarSharpIcon from '@mui/icons-material/PermContactCalendarSharp';
+import DownloadSharpIcon from '@mui/icons-material/DownloadSharp';
 import Dropdown from "react-bootstrap/Dropdown";
 import Resume from "../../Images/Jeff Cameron Resume.pdf";
 
@@ -16,10 +20,10 @@ function DropDownMenu() {
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item href="#about-me">About Me</Dropdown.Item>
-          <Dropdown.Item href="#portfolio">Portfolio</Dropdown.Item>
-          <Dropdown.Item href="#connect">Lets Connect!</Dropdown.Item>
-          <Dropdown.Item target = "_blank" rel="noreferrer" href={Resume} download>Resume</Dropdown.Item>
+          <Dropdown.Item href="#about-me"><AccountCircleOutlinedIcon className="dropdown-icon" /> About Me</Dropdown.Item>
+          <Dropdown.Item href="#portfolio"><AppsOutlinedIcon className="dropdown-icon"/>Portfolio</Dropdown.Item>
+          <Dropdown.Item href="#connect"><PermContactCalendarSharpIcon className="dropdown-icon"/>Lets Connect!</Dropdown.Item>
+          <Dropdown.Item target = "_blank" rel="noreferrer" href={Resume} download> <DownloadSharpIcon className="dropdown-icon"/>Resume</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </div>
